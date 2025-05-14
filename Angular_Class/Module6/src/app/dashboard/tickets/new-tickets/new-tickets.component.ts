ff4140a7489cdd32cfed8bc17896008a2d975185
keagonbeatty <beatt1ks@gmail.com>
2025-05-13 21:17:33 -0500

    almost finished with class for the night

--- Angular_Class/Module6/src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
+++ Angular_Class/Module6/src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ControlComponent } from "../../../shared/control/control.component";
+import { ButtonComponent } from '../../../shared/button/button.component';
 
 @Component({
   selector: 'app-new-tickets',
   standalone: true,
-  imports: [],
+  imports: [ControlComponent, ButtonComponent],
   templateUrl: './new-tickets.component.html',
   styleUrl: './new-tickets.component.css'
 })
