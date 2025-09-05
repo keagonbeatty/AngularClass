9a44ea7f470143dbbd8d7561827c6d222d9d27a5
keagonbeatty <beatt1ks@gmail.com>
2025-09-05 16:07:58 -0700

    worked on class

--- Angular_Class/Module14/01-starting-project/src/app/app.routes.ts
+++ Angular_Class/Module14/01-starting-project/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { TasksComponent } from './tasks/tasks.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -14,13 +15,23 @@ export const routes: Routes = [
     component: UserTasksComponent,
     children: [
       {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'prefix'
+
+      },
+      {
         path: 'tasks', // <your-domain>/users/<uid>/tasks
         component : TasksComponent
       },
       {
         path: 'tasks/new',
         component: NewTaskComponent
-      }
+      },
     ]
   },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
 ];
